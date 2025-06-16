8ad98874eab206d1912c3d9665b554dd96455be0
sinyunju <sinyunju0214@gmail.com>
2025-06-16 10:25:30 +0900

    Update paths.ts

--- src/paths.ts
+++ src/paths.ts
@@ -7,6 +7,20 @@ export const paths = {
     customers: '/dashboard/customers',
     integrations: '/dashboard/integrations',
     settings: '/dashboard/settings',
+    history: '/dashboard/history',
   },
   errors: { notFound: '/errors/not-found' },
 } as const;
+
+/*export const paths = {
+  home: '/',
+  auth: { signIn: '/auth/sign-in', signUp: '/auth/sign-up', resetPassword: '/auth/reset-password' },
+  dashboard: {
+    overview: '/dashboard',
+    account: '/dashboard/account',
+    customers: '/dashboard/customers',
+    integrations: '/dashboard/integrations',
+    settings: '/dashboard/settings',
+  },
+  errors: { notFound: '/errors/not-found' },
+} as const;*/
