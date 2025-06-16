aeabc0829af6ee21edfd7fc278476a811125a884
sinyunju <sinyunju0214@gmail.com>
2025-06-16 18:24:39 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,17 @@
-import { getSiteURL } from '@/lib/get-site-url';
+import type { NavItemConfig } from '@/types/nav';
+import { paths } from '@/paths';
+
+export const navItems = [
+  { key: 'integrations', title: 'CSV Upload', href: paths.dashboard.integrations, icon: 'plugs-connected' },
+  { key: 'settings', title: 'Threshold Input', href: paths.dashboard.settings, icon: 'gear-six' },
+  { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
+  { key: 'customers', title: 'Customers', href: paths.dashboard.customers, icon: 'users' },
+  { key: 'history', title: 'Results History', href: paths.dashboard.history, icon: 'x-square' },
+  { key: 'account', title: 'Send Email', href: paths.dashboard.account, icon: 'user' },
+  { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
+] satisfies NavItemConfig[];
+
+/*import { getSiteURL } from '@/lib/get-site-url';
 import { LogLevel } from '@/lib/logger';
 
 export interface Config {
@@ -9,4 +22,4 @@ export interface Config {
 export const config: Config = {
   site: { name: 'Devias Kit', description: '', themeColor: '#090a0b', url: getSiteURL() },
   logLevel: (process.env.NEXT_PUBLIC_LOG_LEVEL as keyof typeof LogLevel) ?? LogLevel.ALL,
-};
+};*/
